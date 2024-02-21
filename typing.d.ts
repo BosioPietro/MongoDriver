a9cc969dbef163fb746a1d8b00b0151d520c1533
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-02-21 21:26:39 +0100

    Update package version and add typings for MongoDriver class

--- typing.d.ts
+++ typing.d.ts
@@ -1,1 +1,25 @@
-declare module '@bosio/mongodriver';
+import { ObjectId } from "mongodb";
+
+declare module '@bosio/mongodriver' {
+    class MongoDriver {
+        private constructor(strConn: string);
+        public static CreaDatabase(strConn: string, nomeDatabase: string, collezione?: string): Promise<MongoDriver>;
+        public ID(id: string): ObjectId;
+        public SettaCollezione(collezione: string): Promise<void>;
+        public Collezioni(): Promise<{ collezioni?: string[], errore?: string }>;
+        public SettaDatabase(nomeDatabase: string): Promise<void>;
+        public PrendiMolti(query?: object, projection?: object, sort?: { sort: any, direction?: number }): Promise<object>;
+        public PrendiUno(query?: object, projection?: object): Promise<object>;
+        public Inserisci(...oggetti: object[]): Promise<object>;
+        public UpdateUno(filtro: object, update: object, upsert?: boolean): Promise<object>;
+        public UpdateMolti(filtro: object, update: object, upsert?: boolean): Promise<object>;
+        public SostituisciUno(filtro: object, oggetto: object, upsert?: boolean): Promise<object>;
+        public EliminaUno(query: object): Promise<object>;
+        public Elimina(query: object): Promise<object>;
+        public NumeroRecord(query?: object): Promise<object>;
+        public PrendiDistinct(record: string, query?: object): Promise<object>;
+        public Replace(query: object, nuovo: object, upsert?: boolean): Promise<object>;
+    }
+
+    export default MongoDriver;
+}
