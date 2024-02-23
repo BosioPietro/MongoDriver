022526c4f4f25665af6a57f577595caacbd256ba
BosioPietro <p.bosio.2155@vallauri.edu>
2024-02-23 17:24:47 +0100

    Fix typos and update package version

--- index.ts
+++ index.ts
@@ -34,7 +34,7 @@ class MongoDriver{
         await database.SettaDatabase(nomeDatabase);
         if(collezione) await database.SettaCollezione(collezione);
 
-        database.Prompt("Database " + database.database + " e collezione " + database.collezione + "impostati")
+        database.Prompt("Database " + database.database + " e collezione " + database.collezione + " impostati")
         return database;
     }
 
@@ -63,7 +63,7 @@ class MongoDriver{
         {
             this.collezione = collezione;
         }
-        else throw new Error("La collezione " + collezione + "non esiste");
+        else throw new Error("La collezione " + collezione + " non esiste");
     }
 
     /**
@@ -106,7 +106,7 @@ class MongoDriver{
         {
             this.database = nomeDatabase;
         }
-        else throw new Error("Il database" + nomeDatabase + "non esiste");
+        else throw new Error("Il database " + nomeDatabase + " non esiste");
     } 
 
     /**
